8f35922e9911a3e2d527ecb8e1620514162e1118
Lance <lancealan.2021@gmail.com>
2023-01-29 01:50:17 -0500

    Added Routing

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AddPoi from "./components/add_poi"
 import { Auth0Provider } from '@auth0/auth0-react';
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const routes = createBrowserRouter([{
+  path: "/", 
+  element: <App />
+},
+{
+  path: "/add_poi",
+  element: <AddPoi />
+}
+
+])
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -14,7 +28,7 @@ ReactDOM.render(
     clientId = {clientId}
     redirectUri= {window.location.origin}
     >
-    <App />
+    < RouterProvider router={routes}/> 
     </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
