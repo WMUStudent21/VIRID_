b36267406d41e0ea3e3919588abb1c347616ffe5
Mike <michaeljiahui.loh@wmich.edu>
2023-01-28 20:57:21 -0500

    added retrieve function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,39 +19,77 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-function writeVPointData(coordinate, imageURL, difficulty) {
+function writeUserData(userId, name, coordinate, imageURL) {
     const db = getDatabase();
     const reference = ref(db, 'VPoint/' + coordinate);
 
     set(reference, {
+        coordinate: coordinate,
         profile_pic: imageURL,
-        difficulty: difficulty,
-        coordinate: coordinate
+        difficulty: difficulty
     });
 }
 
-writeVPointData("19D034427A72D859759", "myURL", "Easy");
-writeVPointData("19D036519A72D856597", "myURL", "Medium");
+function retrieveData() {
+  const db = getDatabase();
+  const Ref = ref(db, "VPoint/");
 
-// function readVPointData(coordinate, imageURL, difficulty) {
-//     const db = getDatabase();
-//     const reference = ref(db, 'VPoint/' + coordinate);
+  const list = [];
+  onValue(Ref, snapshot => {
+    snapshot.forEach(function(element){
+      var string = element.ref._path.pieces_[1];
+      for (let i = 0; i < 2; i++) {
+        string = string.replace("D",".");
+        string = string.replace("A",",");
+      }
+      string = string.split(',');
+      list.push(string);
+    });
+  });
+  return list;
+};
 
-//     set(reference, {
-//         profile_pic: imageURL,
-//         difficulty: difficulty,
-//         coordinate: coordinate
-//     });
+const x = retrieveData();
+
+
+
+const dummy_start = ["19.021767", "72.857851"]
+// API KEY = AIzaSyCb5zQfEwi3KzCaMDjctIOEX89V5g9joSs
+
+// function distance(current_location,destination_coordinates) {
+//   var origin = new google.maps.LatLng(Number(current_location[0]), Number(current_location[1]));
+//   var destination = new google.maps.LatLng(Number(destination_coordinates[0]), Number(destination_coordinates[1]));
+
+//   var service = new google.maps.DistanceMatrixService();
+//   service.getDistanceMatrix(
+//     {
+//       origins: [origin],
+//       destinations: [destination],
+//       travelMode: 'WALKING',
+//       transitOptions: TransitOptions,
+//       drivingOptions: DrivingOptions,
+//       unitSystem: UnitSystem,
+//       avoidHighways: Boolean,
+//       avoidTolls: Boolean,
+//     }, callback); 
+// };
+
+// function callback(response, status) {
+//   if (status == 'OK') {
+//     var origins = response.originAddresses;
+//     var destinations = response.destinationAddresses;
+
+//     for (var i = 0; i < origins.length; i++) {
+//       var results = response.rows[i].elements;
+//       for (var j = 0; j < results.length; j++) {
+//         var element = results[j];
+//         var distance = element.distance.text;
+//         var duration = element.duration.text;
+//         var from = origins[i];
+//         var to = destinations[j];
+//       }
+//     }
+//   }
 // }
 
-
-// Get a database reference to our posts
-const db = getDatabase();
-const ref1 = db.ref('VPoint');
-
-// Attach an asynchronous callback to read the data at our posts reference
-ref1.on('value', (snapshot) => {
-  console.log(snapshot.val());
-}, (errorObject) => {
-  console.log('The read failed: ' + errorObject.name);
-}); 
+// distance(dummy_start,x[0]);
